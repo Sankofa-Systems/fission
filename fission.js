cd204485773a445b680f71bbb06e1a2c23720100
Gerald Nash <me@aunyks.com>
2019-08-26 15:44:02 -0400

    Add checksum option for requests

--- fission.js
+++ fission.js
@@ -2,32 +2,54 @@ const {
   split,
   combine
 } = require('./vendor/sss')
+const {
+  createHash
+} = require('crypto')
+const sha256 = createHash('sha256')
+
+const splitFn = (req, res) => {
+  const {
+    secret,
+    numShares,
+    numThreshold
+  } = req.body
+  const secretBuffer = Buffer.from(secret, 'hex')
+  const splitShares =
+    split(secretBuffer, { shares: numShares, threshold: numThreshold })
+      .map(shareBuffer => shareBuffer.toString('hex'))
+  res.json({
+    shares: splitShares,
+    checksum: sha256.update(secretBuffer).digest('hex')
+  })
+}
+
+const join = (req, res) => {
+  const {
+    checksum,
+    shares
+  } = req.body
+  const shareBuffers = shares.map(hex => Buffer.from(hex, 'hex'))
+  const joinedSecret = combine(shareBuffers)
+  const joinedSecretHex = joinedSecret.toString('hex')
+  const recomputedDigest = sha256.update(joinedSecret).digest('hex')
+  if (checksum && recomputedDigest !== checksum) {
+    res.status(400)
+    res.end('Checksums did not match')
+  } else {
+    res.json({
+      secret: joinedSecretHex
+    })
+  }
+}
 
 module.exports = (req, res) => {
   try {
     switch (req.url) {
       case '/split':
-        const {
-          secret,
-          numShares,
-          numThreshold
-        } = req.body
-        const secretBuffer = Buffer.from(secret, 'hex')
-        const splitShares =
-          split(secretBuffer, { shares: numShares, threshold: numThreshold })
-            .map(shareBuffer => shareBuffer.toString('hex'))
-        res.json({
-          shares: splitShares
-        })
+        splitFn(req, res)
         break;
       case '/join':
-        const shareBuffers = req.body.shares.map(hex => Buffer.from(hex, 'hex'))
-        const joinedSecret =
-          combine(shareBuffers)
-            .toString('hex')
-        res.json({
-          secret: joinedSecret
-        })
+        join(req, res)
         break;
       default:
         res.status(400)
@@ -35,7 +57,7 @@ module.exports = (req, res) => {
         break;
     }
   } catch (err) {
-    console.log(err)
+    console.error(err)
     res.status(500)
     res.end()
   }
